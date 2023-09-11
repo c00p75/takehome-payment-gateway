0e22f15294abb0ce5716f7ff8fc7b754106d4d56
c00p75_ <georgecoopmsapenda@gmail.com>
2023-09-11 23:56:15 +0200

    Create state for displaying popup payment form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react"
 import Banner from "./components/banner/Banner"
+import DonateBtn from "./components/donateBtn/DonateBtn"
 
 const App = () => {
+  const [showPaymentForm, setShowForm] = useState(false);
+  const setShowPaymentForm = (bool) => {
+    const body = document.querySelector('body');
+    if (bool) { body.style.overflow = 'hidden';}
+    else {body.style.overflow = 'auto';}
+    setShowForm(bool);
+  }
+
   return (
     <div id="app">
       <Banner />
+      <DonateBtn setShowPaymentForm={setShowPaymentForm} showPaymentForm={showPaymentForm} />
     </div>
   )
 }
