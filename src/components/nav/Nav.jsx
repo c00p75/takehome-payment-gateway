0b7bf08c9ac129cd8a28e3eadb963605c9d83821
c00p75_ <georgecoopmsapenda@gmail.com>
2023-09-11 23:40:39 +0200

    Add navigation bar with logo and phone number

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,12 @@
+const Nav = () => {
+  return (
+    <div id="nav">
+      <span className="logo">
+        FutureEd<br />Foundation
+      </span>
+      <span>+260-974-549-983</span>
+    </div>
+  )
+}
+
+export default Nav
