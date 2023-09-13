6d91e7e2b3651a24e9a93ae89ca55e7adbcd7312
c00p75_ <georgecoopmsapenda@gmail.com>
2023-09-13 14:36:53 +0200

    Configure cors

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // NODE JS WEB SERVER
 // Import built-in http built in method
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 // Configure and retrieve environment variables
 dotenv.config();
-
 // Declare port variable
 const port = 3001
 
@@ -14,6 +14,13 @@ const baseUrl = '/api/v1/payment';
 // Create an expressJs instance
 const app = express();
 
+// Use the cors middleware 
+app.use(cors({
+  origin: 'http://localhost:3000',  // Specify the allowed origin for your frontend
+  methods: '*',                     // allowed HTTP methods
+  allowedHeaders: '*',              // allowed headers
+}));
+
 // Make url data more readable
 app.use(express.json());
 
@@ -21,7 +28,6 @@ app.use(express.json());
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 })
-
 
 // Handle post request from front end
 app.post(baseUrl, (req, res) => {
