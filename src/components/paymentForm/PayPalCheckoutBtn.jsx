dee53b2e46d652841857b79abd3bb6f0b3580924
c00p75_ <georgecoopmsapenda@gmail.com>
2023-09-14 14:45:57 +0200

    Ensure that paypal id is hidden

--- src/components/paymentForm/PayPalCheckoutBtn.jsx
+++ src/components/paymentForm/PayPalCheckoutBtn.jsx
@@ -1,37 +1,39 @@
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { PropTypes } from 'prop-types';
 
-const PayPalCheckoutBtn = ({usdAmount, setPayPalStatus}) => {
-  const id = "AVSU4y6mlkdcv6_XHT4DpZ-XivLGkNL4Vmzh-KykJrlJ9CKTBlc-R1qKA7ZsB4rfRVigXxJsQwIhkhN-"
-  console.log(id);
-
+const PayPalCheckoutBtn = ({usdAmount, setPayPalStatus, payPalId}) => {
+  const id = payPalId;
   const handleApprove = () => {
     setPayPalStatus(true);
     document.querySelector('#checkout-form').submit();
   };
   return (
-    <PayPalScriptProvider options={{ "client-id": id}}>
-      <PayPalButtons
-        style={{color: "silver"}}
-        createOrder={(data, actions) => {
-          return actions.order.create({
-            purchase_units: [{
-              description: 'FutureEd Donation',
-                amount: { value: usdAmount }
-            }]
-          })
-        }}
-        onApprove={async(data, actions) => {
-          const order = await actions.order.capture;
-          console.log("Order: ", order)
-          handleApprove();
-        }}
-        onError={(error) => {
-          alert(error);
-          setPayPalStatus(false);
-        }}
-      />
-    </PayPalScriptProvider>
+    <>
+      {id && (
+        <PayPalScriptProvider options={{ "client-id": id}}>
+          <PayPalButtons
+            style={{color: "silver"}}
+            createOrder={(data, actions) => {
+              return actions.order.create({
+                purchase_units: [{
+                  description: 'FutureEd Donation',
+                    amount: { value: usdAmount }
+                }]
+              })
+            }}
+            onApprove={async(data, actions) => {
+              const order = await actions.order.capture;
+              console.log("Order: ", order)
+              handleApprove();
+            }}
+            onError={(error) => {
+              alert(error);
+              setPayPalStatus(false);
+            }}
+          />
+        </PayPalScriptProvider>
+      )}
+    </>
   )
 }
 
@@ -41,6 +43,7 @@ PayPalCheckoutBtn.propTypes = {
     PropTypes.number,
   ]).isRequired,
   setPayPalStatus: PropTypes.func.isRequired,
+  payPalId: PropTypes.string.isRequired,
 };
 
 export default PayPalCheckoutBtn
